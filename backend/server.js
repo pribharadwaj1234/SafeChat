4ebbf5f12386c7f8dc991021bbcd34869afee51c
pribharadwaj1234 <bibliophilicpriya@gmail.com>
2024-11-23 07:42:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://safe-chat-ivory.vercel.app"],
+    methods: ["POST","GET"],
+    credentials: true
+  }
+));
 app.use(bodyParser.json());
 
 // MongoDB connection
